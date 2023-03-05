3bb8e6ee36bee7fc12ca173ec0596651c2067bdc
htdamasio <henriquetome98@gmail.com>
2023-03-05 18:35:28 -0300

    Fix text error

--- components/ReadMockup.tsx
+++ components/ReadMockup.tsx
@@ -10,7 +10,7 @@ export function ReadMockup() {
         alt="cell phone example"
       />
       <div className="max-w-sm px-10 sm:px-0">
-        <h1 className="font-rubik font-bold text-2xl text-center sm:text-left">Read you&apos;re favorite stories anywhere</h1>
+        <h1 className="font-rubik font-bold text-2xl text-center sm:text-left">Read your favorite stories anywhere</h1>
         <p className="font-roboto font-light text-md leading-5 text-center sm:text-left">Take your favorite reads with you wherever you go using our mobile app. With our stories, you&apos;ll stay captivated, whether you&apos;re connected to the internet or off the grid.</p>
       </div>
     </div>
